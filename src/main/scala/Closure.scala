a22a6eef5ef744700b7f2e3d94dcde8b3ad82781
mayaming <mayaming1983@gmail.com>
2015-05-05 23:37:03 -0700

    Closure study

---
+++ src/main/scala/Closure.scala
@@ -0,0 +1,60 @@
+/**
+ * After running "scalac Closure.scala", you'll get the following classes for this piece of code:
+ * Outmost.class
+ * Outmost$Outer.class
+ * Outmost$Outer$Inner.class
+ * Outmost$Outer$Inner$Innest.class
+ * Outmost$Outer$Inner$$anonfun$innerF$1.class
+ * Outmost$Outer$Inner$$anonfun$innerF2$1.class
+ * Outmost$Outer$Inner$ClsInF$1.class
+ * Outmost$Outer$Inner$ClsInF$2.class
+ */
+
+class Outmost {
+
+  class Outer {
+
+    def outerF(a: Any) = a
+
+    class Inner {
+
+      def innerF(l: Seq[Int]) = {
+        class ClsInF(n: Int) {
+        }
+        //Outmost$Outer$Inner$$anonfun$innerF$1
+        l.map(new ClsInF(_))
+      }
+
+      def innerF2(l: Seq[Int]) = {
+        class ClsInF(n: Int) {
+        }
+        //Outmost$Outer$Inner$$anonfun$innerF2$1
+        l.map(outerF)
+
+        //Outmost$Outer$Inner$$anonfun$1
+        val f: Any => Any = outerF
+        println(f.toString())  //-> <function1>
+        println(f.getClass())  //-> Outmost$Outer$Inner$$anonfun$1
+        println(f.getClass().getSuperclass())  //-> class scala.runtime.AbstractFunction1
+        println(f.getClass().getInterfaces().mkString(","))  //-> interface scala.Serializable
+        l.map(f)
+      }
+
+      class Innest {
+      }
+
+    }
+
+  }
+
+}
+
+object Closure {
+  def main(args: Array[String]):Unit = {
+    val outmost = new Outmost
+    val outer = new outmost.Outer
+    val inner = new outer.Inner
+    inner.innerF(0 to 9)
+    inner.innerF2(0 to 9)
+  }
+}
